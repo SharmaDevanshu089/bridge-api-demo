947583f5595f916a341022e63ae1227d834a03fb
Devanshu Sharma <sharma.devanshu089@gmail.com>
2025-11-10 18:22:33 +0530

    Updated Javascript loading logic

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const { invoke } = window.__TAURI__.core;
+const btn = document.getElementById('fetchBtn');
 
 let buttonclick = false;
 function showLoading() {
+  console.log("Debug");
   buttonclick = true;
-  
+  btn.disabled = true;
+btn.innerHTML = '<div class="loader"></div> Loading...';
 }
+document.getElementById('fetchBtn').addEventListener('click', showLoading);
