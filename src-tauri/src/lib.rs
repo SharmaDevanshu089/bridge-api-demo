a5ae43c2a5dab1306b7ae2591debe713bf4645a0
Devanshu Sharma <sharma.devanshu089@gmail.com>
2025-11-10 18:32:21 +0530

    Fixed backend code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
+use reqwest::Client;
+use serde_json::Value;
+
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
@@ -8,7 +10,31 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![fetch_url])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn fetch_url() -> String {
+    println!("Demo");
+    let api_key = "sk-test-xxxx";
+    let customer_id = "cust_demo_123";
+
+    let url = format!(
+        "https://api.sandbox.bridge.xyz/v0/customers/{}/hosted_tos",
+        customer_id
+    );
+
+    let client = Client::new();
+    let res = client
+        .post(&url)
+        .header("Content-Type", "application/json")
+        .header("Api-Key", api_key)
+        .send()
+        .await
+        .expect("request failed");
+
+    let body: Value = res.json().await.expect("invalid json");
+    body["url"].as_str().unwrap().to_string()
 }
